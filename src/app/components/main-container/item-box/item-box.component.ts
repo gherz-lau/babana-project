0585dbb7172556d17c97f7fbc0da3988d31aa367
Larry Gher <larry.gher@gmail.com>
2020-09-07 13:30:53 -0600

    solucionado el problema de la eliminacion incorrecta y la asignacion de id's

--- src/app/components/main-container/item-box/item-box.component.ts
+++ src/app/components/main-container/item-box/item-box.component.ts
@@ -23,7 +23,11 @@ export class ItemBoxComponent implements OnInit {
 
   addNewItem() {
     this.listItems.push({
-      id: this.listItems.length + 1, //cada que se genere un nuevo item/card, debe tomar como id el numero consecutivo
+      //id: this.listItems.length + 1, //cada que se genere un nuevo item/card, debe tomar como id el numero consecutivo
+      id: 
+        this.listItems.length > 0
+          ? this.listItems[this.listItems.length - 1].id + 1
+          : 1,
       image: '',
       title: '', //Fire for flowers
       description: '',
@@ -32,8 +36,15 @@ export class ItemBoxComponent implements OnInit {
   }
 
   killChild(item) {
-    const index = this.listItems.indexOf(item); //creas una variable llamada index, para guardar ahi el id del item que  se va a matar
-    this.listItems.splice(index, 1); //matas el item (orden splice), un solo item, con el index indicado (o sea el id)
+    //const index = this.listItems.indexOf(item); //creas una variable llamada index, para guardar ahi el id del item que  se va a matar
+    //this.listItems.splice(index, 1); //matas el item (orden splice), un solo item, con el index indicado (o sea el id)
+    const index =this.listItems.findIndex (i => i.id == item.id);
+    //array.indexOf busca primitivos en arrays
+    //array.findIndex permite buscar el indice con base en el resultado de una funcion
+    this.listItems.splice(index, 1);
+  
+  
+  
   }
   
 
